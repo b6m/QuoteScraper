e7c4338cf1ae10d14107eeb922c25279b4c749b0
rip <65096390+b6m@users.noreply.github.com>
2022-05-07 11:54:59 +0200

    Update quote.py

--- quote.py
+++ quote.py
@@ -5,21 +5,27 @@ import json
 
 class QuoteScraper:
     def __init__(self):
-        self.base_url = "https://free-quotes-api.herokuapp.com/"
-    
+        self.base_url      = "https://free-quotes-api.herokuapp.com/"
+        self.quotes        = []
+        self.blacklisted   = []
+        self.duplicate     = []
+
     async def get_quote(self):
         async with httpx.AsyncClient() as client:
             while True:
                 response = await client.get(self.base_url)
                 quote = json.loads(response.text)["quote"]
                 print(quote + "\n")
-                blacklisted_chars = ['\ufffd']
-                if any(char in quote for char in blacklisted_chars):
-                    continue
                 with open("quotes.txt", "a") as f:
                     f.write(quote + "\n")
-                
-
+                    self.quotes.append(quote)
+                blacklisted_chars = ['\ufffd']
+                if any(char in quote for char in blacklisted_chars):
+                    self.blacklisted.append(quote)
+                    continue
+                if quote in self.quotes:
+                    self.duplicate.append(quote)
+                    continue
 
 async def run():
     tasks = []
