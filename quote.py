dc8a7f6eaa6042d284e3c97d68959a1393c45d54
rip <65096390+b6m@users.noreply.github.com>
2022-05-07 12:21:55 +0200

    Update quote.py

--- quote.py
+++ quote.py
@@ -6,30 +6,20 @@ import json
 class QuoteScraper:
     def __init__(self):
         self.base_url      = "https://free-quotes-api.herokuapp.com/"
-        self.quotes        = []
-        self.blacklisted   = []
-        self.duplicate     = []
-        self.chars_blacklist = ['\ufffd']
-
+        self.quotes_file   = "quotes.txt"
+    
     async def get_quote(self):
         async with httpx.AsyncClient() as client:
             while True:
-                
+                    
                 response = await client.get(self.base_url)
-                quote = json.loads(response.text)["quote"]
-                print(quote + "\n")
+
+                quote = json.loads(response.text)['quote']
                 
-                with open("quotes.txt", "a") as f:
-                    f.write(quote + "\n")
-                    self.quotes.append(quote)
-                    
-                if any(char in quote for char in self.chars_blacklist):
-                    self.blacklisted.append(quote)
-                    continue
-                    
-                if quote in self.quotes:
-                    self.duplicate.append(quote)
-                    continue
+                with open(self.quotes_file, "a", encoding="utf-8") as handler:
+                    handler.write(f"{quote}\n")
+                    print('\u001b[36;1m[\u001b[0m~\u001b[36;1m] \u001b[0m Quote   • ', quote)
+                    await asyncio.sleep(0.00009)
 
 async def run():
     tasks = []
