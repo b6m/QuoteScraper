4b98a1dd558026205b3e0399837afef910cb698d
rip <65096390+b6m@users.noreply.github.com>
2022-05-07 12:39:18 +0200

    Update quote.py

--- quote.py
+++ quote.py
@@ -1,24 +1,27 @@
-import threading
 import asyncio
 import httpx
 import json
+import random
 
 class QuoteScraper:
     def __init__(self):
-        self.base_url      = "https://free-quotes-api.herokuapp.com/"
+        self.apis          = ["https://free-quotes-api.herokuapp.com/", "https://api.quotable.io/random"]
         self.quotes_file   = "quotes.txt"
     
     async def get_quote(self):
         async with httpx.AsyncClient() as client:
             while True:
-                    
-                response = await client.get(self.base_url)
-
-                quote = json.loads(response.text)['quote']
+                api = random.choice(self.apis)
+                response = await client.get(api)
                 
+                if api == self.apis[0]:
+                    quote = json.loads(response.text)['quote']
+                elif api == self.apis[1]:
+                    quote = json.loads(response.text)['content']
+
                 with open(self.quotes_file, "a", encoding="utf-8") as handler:
                     handler.write(f"{quote}\n")
-                    print('\u001b[36;1m[\u001b[0m~\u001b[36;1m] \u001b[0m Quote   • ', quote)
+                    print(f'\u001b[36;1m[\u001b[0m~\u001b[36;1m] \u001b[0m {api} | Quote   • {quote}')
                     await asyncio.sleep(0.00009)
 
 async def run():
