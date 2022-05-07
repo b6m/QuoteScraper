a26387ce814e4f3ed45a15f6e8ac55c092d3a474
rip <65096390+b6m@users.noreply.github.com>
2022-05-07 11:56:04 +0200

    Update quote.py

--- quote.py
+++ quote.py
@@ -9,20 +9,24 @@ class QuoteScraper:
         self.quotes        = []
         self.blacklisted   = []
         self.duplicate     = []
+        self.chars_blacklist = ['\ufffd']
 
     async def get_quote(self):
         async with httpx.AsyncClient() as client:
             while True:
+                
                 response = await client.get(self.base_url)
                 quote = json.loads(response.text)["quote"]
                 print(quote + "\n")
+                
                 with open("quotes.txt", "a") as f:
                     f.write(quote + "\n")
                     self.quotes.append(quote)
-                blacklisted_chars = ['\ufffd']
-                if any(char in quote for char in blacklisted_chars):
+                    
+                if any(char in quote for char in self.chars_blacklist):
                     self.blacklisted.append(quote)
                     continue
+                    
                 if quote in self.quotes:
                     self.duplicate.append(quote)
                     continue
